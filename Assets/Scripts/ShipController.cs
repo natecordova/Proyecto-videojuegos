3e18cd1184da4e789e911b743e8a326606745fc0
Iván Mera <imera92@gmail.com>
2019-07-15 04:22:01 -0500

    Juguete - mecánicas de movimiento

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            if (gameObject.transform.localScale.x < 1)
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, -90);
+                gameObject.transform.Translate(-5 * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, 90);
+                gameObject.transform.Translate(5 * Time.deltaTime, 0, 0);
+            }
+        }
+        else if (Input.GetKey("down"))
+        {
+            if (gameObject.transform.localScale.x < 1)
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, 90);
+                gameObject.transform.Translate(-5 * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, -90);
+                gameObject.transform.Translate(5 * Time.deltaTime, 0, 0);
+            }
+        }
+        else if (Input.GetKey("left"))
+        {
+            if (gameObject.transform.eulerAngles.z != 0)
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            gameObject.transform.localScale = new Vector3(-1F, 1F, 1F);
+            gameObject.transform.Translate(-5 * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("right"))
+        {
+            if (gameObject.transform.eulerAngles.z != 0)
+            {
+                gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            gameObject.transform.localScale = new Vector3(1F, 1F, 1F);
+            gameObject.transform.Translate(5 * Time.deltaTime, 0, 0);
+        }
+    }
+}
