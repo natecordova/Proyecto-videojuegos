a4f76bd93ee74af6e22431913250bca9b03985c0
Iván Mera <imera92@gmail.com>
2019-07-22 03:55:34 -0500

    Testing level

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShipController : MonoBehaviour
 
 {
+    public float speedX;
+    public float speedY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +18,16 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Movimiento automático en eje X
+        gameObject.transform.Translate(speedX * Time.deltaTime, 0, 0);
+
         if (Input.GetKey("up"))
         {
-            gameObject.transform.Translate(0, 5 * Time.deltaTime, 0);
+            gameObject.transform.Translate(0, speedY * Time.deltaTime, 0);
         }
         else if (Input.GetKey("down"))
         {
-            gameObject.transform.Translate(0, -5 * Time.deltaTime, 0);
+            gameObject.transform.Translate(0, -speedY * Time.deltaTime, 0);
         }
         /*else if (Input.GetKey("left"))
         {
@@ -40,5 +47,12 @@ public class ShipController : MonoBehaviour
             gameObject.transform.localScale = new Vector3(1.5F, 1.5F, 1.5F);
             gameObject.transform.Translate(5 * Time.deltaTime, 0, 0);
         }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag == "spikes") {
+            SceneManager.LoadScene("level 1");
+            Debug.Log("collission");
+        }
     }
 }
