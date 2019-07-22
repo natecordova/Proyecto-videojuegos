c92f4c0bcb99c91f41d755fba76f6a0239301bf1
natecordova <natecordovaortiz@hotmail.com>
2019-07-21 22:32:07 -0500

    Horizontal Scroll background animation, creation asteroids assets and horizontal loop movement

---
+++ Assets/Scripts/MoverAsteroid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverAsteroid : MonoBehaviour
+{
+    
+    public float Speed = 1.0f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+       
+    }
+
+    // Update is called once per frame
+   
+
+    void Update()
+    {
+        transform.Translate(Vector3.left  * Speed * Time.deltaTime);
+    }
+}
