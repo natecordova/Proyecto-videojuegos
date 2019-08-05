84ebaf9e5dc8c3d8e3eea040f720e62b0a15f8a1
Iván Mera <imera92@gmail.com>
2019-08-05 05:46:22 -0500

    Obstacle movement

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float x_speed = 5f;
+    public float x_bound = -45f;
+    private float x_offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        x_offset = Time.deltaTime * x_speed;
+        gameObject.transform.Translate(-x_offset, 0, 0);
+        if (transform.position.x < x_bound)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
