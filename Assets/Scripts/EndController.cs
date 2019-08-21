0700d686cd37dd2694ba17c9f19c9c2e2d9a3cb4
willmont21 <willmont66@gmail.com>
2019-08-21 09:02:09 -0500

    Final scene

---
+++ Assets/Scripts/EndController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndController : MonoBehaviour
+{
+
+    Text flashingText;
+    public string next_scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashingText = GetComponent<Text>();
+        //Call coroutine BlinkText on Start
+        StartCoroutine(BlinkText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            SceneManager.LoadScene(next_scene);
+        }
+    }
+
+    public IEnumerator BlinkText()
+    {
+        //blink it forever. You can set a terminating condition depending upon your requirement
+        while (true)
+        {
+            //set the Text's text to blank
+            flashingText.text = "";
+            //display blank text for 0.5 seconds
+            yield return new WaitForSeconds(.5f);
+            //display “I AM FLASHING TEXT” for the next 0.5 seconds
+            flashingText.text = "Press X to continue";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
